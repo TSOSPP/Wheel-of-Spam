1c335fcf9c7ddeac2e93c30870f965703a211c21
Joshua Herron <herronjo110@live.com>
2018-01-24 21:52:14 -0500

    Add files via upload

--- www/main.js
+++ www/main.js
@@ -1,4 +1,4 @@
-var names = ["Haley", "Josh", "Tebo", "Razzle", "Liz"];
+var names = ["Haley", "Josh", "Tebo", "Razzle", "Liz", "Cynonorph", "Jacob", "Our entire bus"];
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -6,7 +6,7 @@ function sleep(ms) {
 
 async function random() {
 		for (var i = Math.floor((Math.random() * 20) + 1); i >= 0; i--) {
-			document.getElementById("text").innerHTML = names[Math.floor(Math.random() * (names.length - 1))];
+			document.getElementById("text").innerHTML = names[Math.floor(Math.random() * (names.length))];
 			await sleep(62.5);
 		}
 }
